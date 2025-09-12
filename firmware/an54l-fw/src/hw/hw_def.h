755b16890d4928e5c49bd4456d3f3cbbffef5117
Hancheol Cho <chcbaram@gmail.com>
2025-09-12 23:38:04 +0900

    AN54L-FW 추가

---
+++ firmware/an54l-fw/src/hw/hw_def.h
@@ -0,0 +1,61 @@
+#ifndef HW_DEF_H_
+#define HW_DEF_H_
+
+
+#include "bsp.h"
+
+
+#define _DEF_FIRMWATRE_VERSION      "V241115R1"
+#define _DEF_BOARD_NAME             "BARAM-QMK-H7S-FW"
+
+
+
+#define _HW_DEF_RTOS_THREAD_PRI_UART          5
+
+#define _HW_DEF_RTOS_THREAD_MEM_UART          (2*1024)
+
+
+
+#define _USE_HW_LED
+#define      HW_LED_MAX_CH          1
+
+#define _USE_HW_UART
+#define      HW_UART_MAX_CH         1
+#define      HW_UART_CH_SWD         _DEF_UART1
+#define      HW_UART_CH_CLI         HW_UART_CH_SWD
+
+#define _USE_HW_CLI
+#define      HW_CLI_CMD_LIST_MAX    32
+#define      HW_CLI_CMD_NAME_MAX    16
+#define      HW_CLI_LINE_HIS_MAX    8
+#define      HW_CLI_LINE_BUF_MAX    64
+
+#define _USE_HW_CLI_GUI
+#define      HW_CLI_GUI_WIDTH       80
+#define      HW_CLI_GUI_HEIGHT      24
+
+#define _USE_HW_LOG
+#define      HW_LOG_CH              HW_UART_CH_SWD
+#define      HW_LOG_BOOT_BUF_MAX    2048
+#define      HW_LOG_LIST_BUF_MAX    4096
+
+#define _USE_HW_BUTTON
+#define      HW_BUTTON_MAX_CH       BUTTON_PIN_MAX
+
+
+//-- CLI
+//
+#define _USE_CLI_HW_UART            1
+#define _USE_CLI_HW_BUTTON          1
+
+
+typedef enum
+{
+  BTN0,
+  BTN1,
+  BTN2,
+  BUTTON_PIN_MAX,  
+} ButtonPinName_t;
+
+
+#endif
