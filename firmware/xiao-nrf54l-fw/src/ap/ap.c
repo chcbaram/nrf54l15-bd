01d346cf50de91aaef6606daf5f4352c344d8898
Hancheol Cho <chcbaram@gmail.com>
2025-09-28 01:19:31 +0900

    XIAO 보드 추가

---
+++ firmware/xiao-nrf54l-fw/src/ap/ap.c
@@ -0,0 +1,19 @@
+#include "ap.h"
+
+#include <zephyr/logging/log.h>
+
+LOG_MODULE_REGISTER(xaio_nrf54l_fw, LOG_LEVEL_DBG);
+
+
+
+
+void apInit(void)
+{  
+  systemInit();
+}
+
+void apMain(void)
+{
+  systemMain();
+}
+
